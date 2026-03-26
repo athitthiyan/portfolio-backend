47ad967f2d4c36d8e8a7c3341d2f859ba96816a1
athitthiyan <athitthiyananofficial@gmail.com>
2026-03-26 23:25:52 +0530

    update password

--- app/config/settings.py
+++ app/config/settings.py
@@ -2,5 +2,5 @@ import os
 
 DATABASE_URL = os.getenv(
     "DATABASE_URL",
-    "postgresql://postgres:Athitthiyan%40@aws-1-ap-south-1.pooler.supabase.com:6543/postgres?sslmode=require"
+    "postgresql://postgres:Athitthiyan24%40@aws-1-ap-south-1.pooler.supabase.com:6543/postgres?sslmode=require"
 )
